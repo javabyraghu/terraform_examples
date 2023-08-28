997a09cb5a6e4dba870cb7eef92792f17605ca98
RAGHU SIR <javabyraghu@gmail.com>
2023-08-28 14:18:42 +0530

    first commit

---
+++ 02iam-user.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_iam_user" "user-raghu" {
+  name = "raghu"
+  tags = {
+    Description = "IAM User with Admin Access"
+  }
+
+}
+
+resource "aws_iam_policy" "userAdminPolicy" {
+  name   = "AdminUser"
+  policy = file("02iam-policies.json")
+}
+
+resource "aws_iam_user_policy_attachment" "raghu_admin_access" {
+  user       = aws_iam_user.user-raghu.name
+  policy_arn = aws_iam_policy.userAdminPolicy.arn
+}
